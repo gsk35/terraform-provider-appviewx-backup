6fc20dcc8f5079a8dffb0b429c290d63b0284961
Dhivya Venkitapathy <128375351+DhivyaVenkitapathy@users.noreply.github.com>
2024-07-02 18:49:11 +0530

    Update createCertificate.tf

--- appviewx/sampleTFFiles/createCertificate.tf
+++ appviewx/sampleTFFiles/createCertificate.tf
@@ -1,9 +1,9 @@
 provider "appviewx"{
-  	appviewx_username="admin"
-	appviewx_password="AppViewX@121"
+  	appviewx_username=""
+	appviewx_password=""
 	appviewx_environment_is_https=true
-	appviewx_environment_ip="192.168.150.192"
-	appviewx_environment_port="31443"
+	appviewx_environment_ip=""
+	appviewx_environment_port=""
 }
 
 resource "appviewx_create_certificate" "sampletest"{
@@ -13,14 +13,14 @@ resource "appviewx_create_certificate" "sampletest"{
      	bit_length="2048"
      	certificate_authority="Microsoft Enterprise"
      	ca_setting_name="microsoft enterprise"
-     	#dns_names=["appviewx.com","appviewx123.com"]
+     	#dns_names=["test.com","test123.com"]
      	validity_days=365
      	custom_fields={"test":"msca"}
-     	vendor_specific_fields={"templateName":"WebServer"}
+     	vendor_specific_fields={"templateName":"template"}
      	is_sync=true
      	
      	#if is_sync is true
-     	certificate_download_path="/home/dhivya.v/external_projects/projects/terraform_provider/certs"
+     	certificate_download_path="/home/certs"
      	certificate_download_format="PEM"
      	certificate_download_password=""
      	certificate_chain_required=true
