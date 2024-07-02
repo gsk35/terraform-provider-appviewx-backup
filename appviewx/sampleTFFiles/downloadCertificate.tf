a182821188ec92453985db1134a38db44cbaa371
Dhivya Venkitapathy <128375351+DhivyaVenkitapathy@users.noreply.github.com>
2024-07-02 18:51:12 +0530

    Update downloadCertificate.tf

--- appviewx/sampleTFFiles/downloadCertificate.tf
+++ appviewx/sampleTFFiles/downloadCertificate.tf
@@ -1,16 +1,16 @@
 provider "appviewx"{
-  	appviewx_username="admin"
-	appviewx_password="AppViewX@121"
+  	appviewx_username=""
+	appviewx_password=""
 	appviewx_environment_is_https=true
-	appviewx_environment_ip="192.168.150.192"
-	appviewx_environment_port="31443"
+	appviewx_environment_ip=""
+	appviewx_environment_port=""
 }
 
 resource "appviewx_download_certificate" "downloadCert"{
-	resource_id="64100a3d5e634315ee9bb536"
-     	certificate_download_path="/home/dhivya.v/external_projects/projects/terraform_provider/certs"
+	resource_id=""
+     	certificate_download_path="/home/certs"
      	certificate_download_format="P12"
-     	certificate_download_password="AppViewX@123"
+     	certificate_download_password=""
      	certificate_chain_required=true
      	
      	#Password field is only mandatory for the formats (PFX, P12, JKS)
